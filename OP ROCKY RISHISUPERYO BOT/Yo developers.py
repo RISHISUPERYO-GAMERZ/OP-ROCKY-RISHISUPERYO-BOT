9c678f8c139341a9151eee907186e5880673166f
RISHISUPERYO-GAMERZ <73427470+RISHISUPERYO-GAMERZ@users.noreply.github.com>
2021-02-26 23:41:26 +0530

    Create Yo developers.py

---
+++ OP ROCKY RISHISUPERYO BOT/Yo developers.py
@@ -0,0 +1,5 @@
+#DEVELOPERS of the bot and the Owners of the bot
+#                  Developers: @Rishisuperyo
+#                            : @PYTHON_CODER_SRINIVAS
+#                  Owner: @Rishisuperyo
+#                  Owner 2:Rocky_8218                 

1fe30d987e9a2170c70780664b6444f8481f1c1b
RISHISUPERYO-GAMERZ <73427470+RISHISUPERYO-GAMERZ@users.noreply.github.com>
2021-02-27 11:14:13 +0530

    Update Friends.py

--- Friends.py
+++ Friends.py
@@ -1,7 +1,7 @@
 #                 besties list
 # 1.ROCKY
 # 2.KEINSHIN + Srinivas
-'''More 8 besties list left😂''''
+#More 8 besties list left😂
 
                friends list now😎
 #DIPESH,SAHAJ,ROHAN,WHITE FF, JASY ,KIDOO,DEVIL,MBL OWNER, ADITYA VINCHAKKAR .......... AREE BESHARMI KI BHI HAD HAE YAAR😂😂 AUR 483 BAAKI HAE ,YEH BHI LIKHKE DU😂😂

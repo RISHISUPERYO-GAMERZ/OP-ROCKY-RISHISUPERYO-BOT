e0cb61b93c4d9eef0b25a81113e901e47557fd8a
RISHISUPERYO-GAMERZ <73427470+RISHISUPERYO-GAMERZ@users.noreply.github.com>
2021-02-27 12:45:56 +0530

    Update __init__.py

--- RISHISUPERYO/__init__.py
+++ RISHISUPERYO/__init__.py
@@ -205,7 +205,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from RISHISUPERYO.modules.helper_funcs.handlers import (CustomCommandHandler,
+from PYTHON.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 

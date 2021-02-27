7aa4c0427326d2e67c8aa87afba8368b3b76c8f2
RISHISUPERYO-GAMERZ <73427470+RISHISUPERYO-GAMERZ@users.noreply.github.com>
2021-02-27 11:45:44 +0530

    Delete OP ROCKY RISHISUPERYO BOT directory

--- OP ROCKY RISHISUPERYO BOT/Yo developers.py
+++
@@ -1,5 +0,0 @@
-#DEVELOPERS of the bot and the Owners of the bot
-#                  Developers: @Rishisuperyo
-#                            : @PYTHON_CODER_SRINIVAS
-#                  Owner: @Rishisuperyo
-#                  Owner 2:Rocky_8218                 

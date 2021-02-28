8e65aec3fa533fe0084355e976c3e7a10f6fc6fd
RISHISUPERYO-GAMERZ <73427470+RISHISUPERYO-GAMERZ@users.noreply.github.com>
2021-02-28 13:23:24 +0530

    Update __init__.py

--- RISHISUPERYO/__init__.py
+++ RISHISUPERYO/__init__.py
@@ -114,7 +114,7 @@ else:
     JOIN_LOGGER = Config.JOIN_LOGGER
     OWNER_USERNAME = Config.OWNER_USERNAME
 
-    try:
+ " try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
     except ValueError:
